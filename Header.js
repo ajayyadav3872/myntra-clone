d269023eeab70468e15c5bfb0e1027972f7fb245
Ajay Yadav <121283256+ajayyadav3872@users.noreply.github.com>
2023-04-09 01:29:41 +0530

    added header

--- Header.js
+++ Header.js
@@ -2,22 +2,26 @@ import '../App.css';
 
 function Header() {
   return (
-    <div className='flex shopping-cart'>
-      <div className='left'>
-        <div className='column1'>MYNTRA</div>
-        <div className='column2'>MEN</div>
-        <div className='column3'>WOMEN</div>
-        <div className='column4'>KIDS</div>
-        <div className='column5'>HOME & LIVING</div>
-        <div className='column6'>BEAUTY</div>
-        <div className='column7'>STUDIO <sup className='top'>NEW</sup></div>
-        <form className='column8'>
-          <input type="text" placeholder='search for product,brands and more' />
-        </form>
-        <div className='column9'>Proflie</div>
-        <div className='column10'>Wishlist</div>
-        <div className='column11'>Bag</div>
-      </div>
+    <div className='flex'>
+      {
+        <div className='left'>
+          <div className='column1'>
+            <img src='https://images.indianexpress.com/2021/01/myntra.png'/>
+          </div>
+          <div className='column2'>MEN</div>
+          <div className='column3'>WOMEN</div>
+          <div className='column4'>KIDS</div>
+          <div className='column5'>HOME & LIVING</div>
+          <div className='column6'>BEAUTY</div>
+          <div className='column7'>STUDIO <sup className='top'>NEW</sup></div>
+          <form className='column8'>
+            <input type="text" placeholder='search for product,brands and more' />
+          </form>
+          <div className='column9'>Proflie</div>
+          <div className='column10'>Wishlist</div>
+          <div className='column11'>Bag</div>
+        </div>
+      }
     </div>
   );
 }
