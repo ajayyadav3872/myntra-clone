c501b820e90ed8b198d4243de64047e537edb7d5
Ajay Yadav <121283256+ajayyadav3872@users.noreply.github.com>
2023-04-09 10:23:57 +0530

    Added sideBar.js

---
+++ SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SideBar.css";
+
+const SideBar = () => {
+  return (
+    <div className='slider'>
+      <img src='https://cdn1.desidime.com/topics/photos/1559813/original/Screenshot2023-04-06114639.png?1680761942'/>
+    </div>
+  )
+}
+
+export default SideBar;
